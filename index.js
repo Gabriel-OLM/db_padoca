eb76311e870b80a512ec80731495404f34ac93b1
Gabriel de Oliveira Mendonça <127889320+Gabriel-OLM@users.noreply.github.com>
2024-11-11 22:07:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 
 server.db = router.db
-app.use(auth)
-app.use(router)
-app.listen(3000)
+server.use(auth)
+server.use(router)
+server.listen(3000)
 
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 3000; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
