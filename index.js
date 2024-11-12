0ac983951da489bfced1316796995a3e36287786
Gabriel de Oliveira Mendonça <127889320+Gabriel-OLM@users.noreply.github.com>
2024-11-11 22:03:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const jsonServer = require("json-server"); // importing json-server library
+const auth = require('json-server-auth')
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+
+server.db = router.db
+app.use(auth)
+app.use(router)
+app.listen(3000)
+
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
